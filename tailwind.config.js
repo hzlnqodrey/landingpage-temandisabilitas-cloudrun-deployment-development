b19c78e23b14c2364bd6b8bc7576ffcb8ee012c4
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-12 11:45:09 +0700

    update changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     colors: {
       'white': "#fff", 
       'black': '#000',
       'grey': '#686868',
+      'light-grey': '#F5F5F5',
       'blue1': '#007EDA',
       'blue2': '#0162A8',
       'yellow1': '#F7BD4A',
