c4d6c3ebb3ab716e79fe5af0af3b19394cf844a6
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-12 12:53:36 +0700

    fixing api route

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express()
 
 router.get('/', (req, res) => {
-    res.status(200)
     res.send("")
-    res.redirect('/')
 })
 
 module.exports = router
